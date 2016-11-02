55d3ac72bda7a273367e9d128c1de9bb68828d48
AndyScherzinger <info@andy-scherzinger.de>
2016-11-02 13:12:38 +0100

    fix API call after rebase

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -1731,7 +1731,7 @@ public class FileDisplayActivity extends HookActivity
     }
 
     private void tryStopPlaying(OCFile file){
-        if (mMediaServiceConnection != null && file.isAudio()){
+        if (mMediaServiceConnection != null && MimeTypeUtil.isAudio(file)){
             if (mMediaServiceBinder.isPlaying(file)){
                 mMediaServiceBinder.pause();
             }
